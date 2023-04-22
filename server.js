bed927dea53591485bb66b77a987b4e6b860124d
Martin Lynx <martilve@tktk.ee>
2023-04-22 20:06:26 +0300

    add search

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res)=>{
     let url = 'https://api.themoviedb.org/3/movie/157336?api_key=e2fe88f3e56f7f705819913231df438e';
@@ -32,6 +33,57 @@ app.get('/', (req, res)=>{
             poster: posterUrl});
     });
 
+});
+
+app.get('/search', (req, res) => {
+    res.render('search', {movieData:''});
+});
+
+app.post('/search', (req, res) => {
+    let movieTitle = req.body.movieTitle;
+
+    let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=e2fe88f3e56f7f705819913231df438e&query=${movieTitle}`;
+    let genres = 'https://api.themoviedb.org/3/genre/movie/list?api_key=e2fe88f3e56f7f705819913231df438e&language=en-US';
+   
+    let endpoints = [movieUrl, genres];
+
+    axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
+    .then(
+        axios.spread((movie, genres) => {
+            const [general, generalComingSoon] = movie.data.results;
+            const movieGenreIds = general.genre_ids;        
+            const movieGenres = genres.data.genres; 
+            
+            let genresArray = [];
+            for(let i = 0; i < movieGenreIds.length; i++) {
+                for(let j = 0; j < movieGenres.length; j++) {
+                    if(movieGenreIds[i] === movieGenres[j].id) {
+                        console.log(movieGenres[j].name);
+                        genresArray.push(movieGenres[j].name)
+                    }
+                    
+                }
+            }
+
+            let genresToDisplay = '';
+            genresArray.forEach(genre => {
+                genresToDisplay = genresToDisplay+ `${genre}, `;
+            });
+
+            genresToDisplay = genresToDisplay.slice(0, -2) + '.';
+            
+            let movieObject = {
+                title: general.original_title,
+                year: new Date(general.release_date).getFullYear(),
+                overview: general.overview,
+                posterUrl: `https://image.tmdb.org/t/p/w500/${general.poster_path}`,
+                genres: genresToDisplay
+
+            };
+            res.render('search', {movieData: movieObject});
+        })
+      );
+    
 });
 
 app.listen(process.env.PORT || 3000, ()=> {
